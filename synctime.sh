96b9a6177c70cdc9ffcf0c3785685ce47c94cc66
Bui Tan Viet <buitanviet@gmail.com>
2013-11-01 15:32:57 +0700

    Delete synctime.sh

--- synctime.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#script cap nhat gio cho he thong
-#vi chua ccnfig firewall allow port ntpd
-# nen khi gio sai, tam thoi chay script nay
-service iptables stop
-ntpdate pool.ntp.org
-service iptables start
-./iptable.sh
-echo "Done..."
